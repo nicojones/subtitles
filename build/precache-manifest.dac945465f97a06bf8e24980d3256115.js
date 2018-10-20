cef99b08c9cf9bde99b38dd8bdf19749720f86be
Nico Jones Gödel <jones.godel@gmail.com>
2018-10-20 20:34:40 +0200

    added build folder

---
+++ build/precache-manifest.dac945465f97a06bf8e24980d3256115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "82f8b89b27978ff57f4c",
+    "url": "/static/js/main.82f8b89b.chunk.js"
+  },
+  {
+    "revision": "0c0b2acd3f58316e96d2",
+    "url": "/static/js/1.0c0b2acd.chunk.js"
+  },
+  {
+    "revision": "82f8b89b27978ff57f4c",
+    "url": "/static/css/main.5f596f13.chunk.css"
+  },
+  {
+    "revision": "0c0b2acd3f58316e96d2",
+    "url": "/static/css/1.404f45b8.chunk.css"
+  },
+  {
+    "revision": "3623120ce06ea4de420796cf98410800",
+    "url": "/index.html"
+  }
+];
